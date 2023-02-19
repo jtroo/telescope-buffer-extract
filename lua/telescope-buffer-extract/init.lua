76527f0d7803e859b9567956eb8e7307276dd5a4
Jan Tache <j.andreitabs@gmail.com>
2023-02-18 23:26:04 -0800

    refactor+fix: add close fn, DRY

--- lua/telescope-buffer-extract/init.lua
+++ lua/telescope-buffer-extract/init.lua
@@ -4,7 +4,7 @@ local action_state = require "telescope.actions.state"
 local finders = require "telescope.finders"
 local conf = require("telescope.config").values
 
-local set_plus_register_no_insert = function(prompt_bufnr)
+local set_plus_register = function(prompt_bufnr)
   local selection = action_state.get_selected_entry()
   if selection == nil then
     actions.close(prompt_bufnr)
@@ -14,14 +14,7 @@ local set_plus_register_no_insert = function(prompt_bufnr)
   actions.close(prompt_bufnr)
 end
 
-local set_plus_register = function(prompt_bufnr)
-  set_plus_register_no_insert(prompt_bufnr)
-  vim.schedule(function()
-    vim.cmd [[startinsert]]
-  end)
-end
-
-local put_no_insert = function(prompt_bufnr)
+local put = function(prompt_bufnr)
   local selection = action_state.get_selected_entry()
   if selection == nil then
     actions.close(prompt_bufnr)
@@ -33,11 +26,28 @@ local put_no_insert = function(prompt_bufnr)
   end)
 end
 
-local put = function(prompt_bufnr)
-  put_no_insert(prompt_bufnr)
-  vim.schedule(function()
-    vim.cmd [[startinsert]]
-  end)
+local pick = function(title, opts, results)
+  local finder = finders.new_table {
+    results = results,
+  }
+  if opts.no_insert_mode ~= true then
+    finder.close = function()
+      vim.schedule(function()
+        vim.cmd [[startinsert]]
+      end)
+    end
+  end
+  pickers.new({}, {
+    prompt_title = title,
+    finder = finder,
+    sorter = conf.generic_sorter({}),
+    attach_mappings = function(_, map)
+      actions.select_default:replace(set_plus_register)
+      map("i", "<Tab>", put)
+      map("n", "<Tab>", put)
+      return true
+    end,
+  }):find()
 end
 
 local pick_word = function(opts)
@@ -52,25 +62,7 @@ local pick_word = function(opts)
       end
     end
   end
-  pickers.new({}, {
-    prompt_title = "Pick word - Tab: paste, Enter: copy to clipboard",
-    finder = finders.new_table {
-      results = results,
-    },
-    sorter = conf.generic_sorter({}),
-    attach_mappings = function(_, map)
-      if opts.no_insert_mode == true then
-        actions.select_default:replace(set_plus_register_no_insert)
-        map("i", "<Tab>", put_no_insert)
-        map("n", "<Tab>", put_no_insert)
-      else
-        actions.select_default:replace(set_plus_register)
-        map("i", "<Tab>", put)
-        map("n", "<Tab>", put)
-      end
-      return true
-    end,
-  }):find()
+  pick("Pick word - Tab: paste, Enter: copy to clipboard", opts, results)
 end
 
 local pick_line = function(opts)
@@ -83,25 +75,7 @@ local pick_line = function(opts)
       table.insert(results, line)
     end
   end
-  pickers.new(opts, {
-    prompt_title = "Pick line - Tab: paste, Enter: copy to clipboard",
-    finder = finders.new_table {
-      results = results,
-    },
-    sorter = conf.generic_sorter(opts),
-    attach_mappings = function(_, map)
-      if opts.no_insert_mode == true then
-        actions.select_default:replace(set_plus_register_no_insert)
-        map("i", "<Tab>", put_no_insert)
-        map("n", "<Tab>", put_no_insert)
-      else
-        actions.select_default:replace(set_plus_register)
-        map("i", "<Tab>", put)
-        map("n", "<Tab>", put)
-      end
-      return true
-    end,
-  }):find()
+  pick("Pick line - Tab: paste, Enter: copy to clipboard", opts, results)
 end
 
 return {
